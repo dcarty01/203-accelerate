92eae073c68aa8e2f7fb1a996e438b9450fd3e92
Denise Carty <denisecarty@comcast.net>
2020-05-28 16:16:29 -0400

    updates to front page css style

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -60,7 +60,7 @@
    			</div>
 
           <section class="twitter-feed">
-            <h3><Recent Tweet</h3>
+            <h3>RECENT TWEET</h3>
              <?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
                 <div id="secondary" class="widget-area" role="complementary">
              	    <?php dynamic_sidebar( 'sidebar-2' ); ?>
